4718453cf48c35348002c43395dc4561e0d698cc
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2020-12-08 20:10:58 -0300

    Update condicao2.js

--- aula1/javascript/condicao2.js
+++ aula1/javascript/condicao2.js
@@ -1,8 +1,8 @@
-let idade = 16
+let idade = 22
 if (idade < 16) {
     console.log('não vota')
-} else {
-    if (idade >= 16 && idade < 18) {
-        console.log('voto não obrigatorio')
-    }
+} else if (idade >= 16 && idade < 18) {
+    console.log('voto não obrigatorio')
+} else if (idade >= 18) {
+    console.log('voto obrigaorio')
 }
