a6282698543e296bdaa86c05ec796630ede1e99d
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2020-12-08 21:29:09 -0300

    Create switch.js

---
+++ aula1/javascript/switch.js
@@ -0,0 +1,29 @@
+//switch é obrigqatorio break em todos os cases
+//e defaultao final pra retornar
+let agora = new Date()
+let diaSem = agora.getDay()
+switch (diaSem) {
+    case 0:
+        console.log('Domingo')
+        break;
+    case 1:
+        console.log('Segunda')
+        break;
+    case 2:
+        console.log('Terça')
+        break;
+    case 3:
+        console.log('quarta')
+        break;
+    case 4:
+        console.log('quinta')
+        break;
+    case 5:
+        console.log('sexta')
+        break;
+    case 6:
+        console.log('sábado')
+        break;
+    default:
+        break;
+}
