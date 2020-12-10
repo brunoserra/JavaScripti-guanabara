efe16e4c96f27a4ed3481aace39884c7344cae02
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2020-12-09 21:46:01 -0300

    criando

--- treino/treino.js
+++ treino/treino.js
@@ -6,12 +6,15 @@ function carregar(params) {
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         img.src = 'imagens/manha.png'
+        document.body.style.backgroundColor = '#FFE4B5'
             //bom dia
     } else if (hora >= 12 && hora < 18) {
         //boa tarde
         img.src = 'imagens/tarde.png'
+        document.body.style.backgroundColor = '#D2691E'
     } else {
         img.src = 'imagens/noite.png'
+        document.body.style.backgroundColor = '#191970'
 
         //boa noite
 
