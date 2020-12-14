5442aab74df40d51bb4214e0e00531f44fe3c7af
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2020-12-14 20:58:33 -0300

    ajustando

--- repetição/exrceicios/ambiente.js
+++ repetição/exrceicios/ambiente.js
@@ -3,6 +3,10 @@ function contar() {
     let passo = document.getElementById('txtp')
     let fim = document.getElementById('txtf')
 
-
+    if (ini.value.length == 0 || passo.value.length == 0 || fim.value.length == 0) {
+        alert('[Erro] digite um valor ')
+    } else {
+        alert('tudo certo')
+    }
 
 }
