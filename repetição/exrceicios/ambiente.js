51364361cbf84c12d8eac39140ddf3b868ccca24
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2020-12-14 21:45:21 -0300

    Update ambiente.js

--- repetição/exrceicios/ambiente.js
+++ repetição/exrceicios/ambiente.js
@@ -1,13 +1,22 @@
 function contar() {
-    let ini = document.getElementById('txti')
-    let passo = document.getElementById('txtp')
+    let inicio = document.getElementById('txti')
     let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
     let res = document.getElementById('res')
-    if (ini.value.length == 0 || passo.value.length == 0 || fim.value.length == 0) {
+    if (inicio.value.length == 0 || passo.value.length == 0 || fim.value.length == 0) {
         alert('[Erro] digite um valor ')
     } else {
-        res.innerHTML = 'contando...'
+        res.innerHTML = 'contando:'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (i < f) {
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F422} `
+            }
+        }
 
+        res.innerHTML += `\u{1F3B8}`
     }
 
 }
