edcd79d1ae7a452371c8c20d72c31d3d131dd340
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2020-12-16 19:01:27 -0300

    tabuada

--- repetição/tabuada/tabuada.js
+++ repetição/tabuada/tabuada.js
@@ -6,6 +6,13 @@ function tabuada() {
         alert('Por favor digite um numero')
     } else {
         let n = Number(num.value)
+        let c = 1
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${num} x ${c} = ${num*c}`
+            tab.appendChild(item)
+            c++
+        }
     }
 
 
