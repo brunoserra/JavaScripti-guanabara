50b5567d283db6b1d2f1e2845581a7567e36ee52
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2020-12-16 20:13:41 -0300

    Update ambiente.js

--- varcomposta/ambiente.js
+++ varcomposta/ambiente.js
@@ -2,6 +2,15 @@ let num = [5, 7, 8, 3]
     //num[4] = 12 acrescentar elemente
 num.push(6)
 num.sort()
-
-
 console.log(num)
+let pos = num.indexOf(3)
+console.log(`o valor 3 esta na posição ${pos}`)
+    /*
+    for (let pos = 0; pos < num.length; pos++) {
+        console.log(num[pos])
+    */
+    //} //percurso para vetor acima
+    //Forma mais simples
+for (let pos in num) {
+    console.log(num[pos])
+}
