c726bf713b7fb8a07d437481205c53e5150452a7
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2021-01-02 18:55:25 -0300

    limpar

--- exercicio/treinos.js
+++ exercicio/treinos.js
@@ -25,7 +25,40 @@ function adicionar() {
         let item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado`
         lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         alert('não é um numero ou valor já adicionado')
     }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length == 0) {
+        alert('Adicione valor antes de finalizar')
+    } else {
+        let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for (let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior)
+                maior = valores[pos]
+            if (valores[pos] < menor)
+                menor = valores[pos]
+        }
+
+        media = soma / total
+        res.innerHTML = ''
+        res.innerHTML += `<p> ao todo temos ${total} números </p>`
+        res.innerHTML += `<p> o maior número é ${maior} </p>`
+        res.innerHTML += `<p> o menor número é ${menor} </p>`
+        res.innerHTML += `<p> o resultado da soma é ${soma} </p>`
+        res.innerHTML += `<p> o resultado da soma é ${media} </p>`
+
+    }
+
+
 }
